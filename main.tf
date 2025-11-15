5559d9d1dcb08bc3dae5a2cd25f2ae8b60c3701d
iam-sirisha <167849090+iam-sirisha@users.noreply.github.com>
2025-11-15 16:41:19 +0530

    adding one more resource

--- main.tf
+++ main.tf
@@ -19,3 +19,11 @@ resource "aws_instance" "my_ec2" {
     Name = "Terraform-EC2"
   }
 }
+resource "aws_instance" "second_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terra-EC2"
+  }
+}
