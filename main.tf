16832619564e34687866f3e7f48572a68c3c759f
iam-sirisha <167849090+iam-sirisha@users.noreply.github.com>
2025-11-15 15:58:25 +0530

    creating an ec2 instance

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
